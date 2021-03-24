6097b3c0401c0f7889c21003141e7fc81b5fcace
Rafal Cymerys <rafal@upsidelab.io>
2021-03-24 11:20:27 +0100

    Add billing address form

--- packages/composables/src/useUserShipping/index.ts
+++ packages/composables/src/useUserShipping/index.ts
@@ -4,57 +4,6 @@ import {
   UseUserShippingFactoryParams
 } from '@vue-storefront/core';
 
-const addresses: any[] = [
-  {
-    id: 1,
-    email: 'john@gmail.com',
-    firstName: 'John',
-    lastName: 'Doe',
-    streetName: 'Warsawska',
-    apartment: '24/193A',
-    city: 'Phoenix',
-    state: null,
-    zipCode: '26-620',
-    country: 'US',
-    phoneNumber: '560123456',
-    isDefault: true
-  },
-  {
-    id: 2,
-    email: 'havaka@gmail.com',
-    firstName: 'Jonatan',
-    lastName: 'Doe',
-    streetName: 'Starachowicka',
-    apartment: '20/193A',
-    city: 'Atlanta',
-    state: null,
-    zipCode: '53-603',
-    country: 'US',
-    phoneNumber: '560123456',
-    isDefault: false
-  }
-];
-
-const shipping = {
-  addresses
-};
-
-const disableOldDefault = () => {
-  const oldDefault = addresses.find(address => address.isDefault);
-  if (oldDefault) {
-    oldDefault.isDefault = false;
-  }
-};
-
-const sortDefaultAtTop = (a, b) => {
-  if (a.isDefault) {
-    return -1;
-  } else if (b.isDefault) {
-    return 1;
-  }
-  return 0;
-};
-
 const params: UseUserShippingFactoryParams<any, any> = {
   addAddress: async (context: Context, params?) => {
     await context.$spree.api.addAddress(params.address);
@@ -62,16 +11,10 @@ const params: UseUserShippingFactoryParams<any, any> = {
     return { addresses };
   },
 
-  deleteAddress: async (context: Context, params?) => {
-    console.log('Mocked: deleteAddress', params);
-
-    const indexToRemove = addresses.findIndex(address => address.id === params.address.id);
-    if (indexToRemove < 0) {
-      return Promise.reject('This address does not exist');
-    }
-
-    addresses.splice(indexToRemove, 1);
-    return Promise.resolve(shipping);
+  deleteAddress: async (context: Context) => {
+    console.log('Not implemented: Delete shipping address');
+    const addresses = await context.$spree.api.getAddresses();
+    return { addresses };
   },
 
   updateAddress: async (context: Context, params?) => {
@@ -80,26 +23,15 @@ const params: UseUserShippingFactoryParams<any, any> = {
     return { addresses };
   },
 
-  load: async (context: Context, _params?) => {
+  load: async (context: Context) => {
     const addresses = await context.$spree.api.getAddresses();
-    return { addresses }
+    return { addresses };
   },
 
-  setDefaultAddress: async (context: Context, params?) => {
-    console.log('Mocked: setDefault');
-    const isDefault = id => addresses[0].id === id;
-
-    if (!isDefault(params.address.id)) {
-      const indexToUpdate = addresses.findIndex(address => address.id === params.address.id);
-      if (indexToUpdate < 0) {
-        return Promise.reject('This address does not exist');
-      }
-      disableOldDefault();
-      addresses[indexToUpdate].isDefault = true;
-      addresses.sort(sortDefaultAtTop);
-    }
-
-    return Promise.resolve(shipping);
+  setDefaultAddress: async (context: Context) => {
+    console.log('Not implemented: Set default shipping address');
+    const addresses = await context.$spree.api.getAddresses();
+    return { addresses };
   }
 };
 
