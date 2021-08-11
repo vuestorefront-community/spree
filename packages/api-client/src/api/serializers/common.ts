b0d69043e53874cdfd4079cf2c786313b7de526d
Daniel Hajduk <daniel.hajduk@upsidelab.io>
2021-08-11 12:01:34 +0200

    Update fetching categories and products

--- packages/api-client/src/api/serializers/common.ts
+++ packages/api-client/src/api/serializers/common.ts
@@ -2,6 +2,7 @@ export const filterAttachments = (attachments, type, ids) =>
   attachments.filter((e) => e.type === type && ids.includes(e.id));
 
 export const extractRelationships = (attachments, type, relationship, item) => {
+  if (!item.relationships[relationship]) return [];
   const relationships = item.relationships[relationship].data;
 
   return Array.isArray(relationships)
