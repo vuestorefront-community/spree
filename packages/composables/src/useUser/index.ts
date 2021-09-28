d00c9f80263f528d1b70f73fdbf6b072eddecfe9
Rafal Cymerys <rafal@upsidelab.io>
2021-09-28 17:45:14 +0200

    Parse API errors when login and registration

--- packages/composables/src/useUser/index.ts
+++ packages/composables/src/useUser/index.ts
@@ -7,6 +7,7 @@ import {
 } from '@vue-storefront/core';
 import { User } from '../types';
 import useCart from '../useCart';
+import { handleApiErrorResponse, extractApiErrorSummary } from '../utils/error';
 
 const params: UseUserFactoryParams<User, any, any> = {
   provide() {
@@ -33,19 +34,31 @@ const params: UseUserFactoryParams<User, any, any> = {
   },
 
   register: async (context: Context, { email, password, firstName, lastName }) => {
-    await context.$spree.api.registerUser({ email, password, firstName, lastName });
+    try {
+      await context.$spree.api.registerUser({ email, password, firstName, lastName });
 
-    return {};
+      return {};
+    } catch (e) {
+      handleApiErrorResponse(e);
+    }
   },
 
   logIn: async (context: Context, { username, password }) => {
-    const guestCartToken = context.cart?.value?.token;
-    await context.$spree.api.logIn({ username, password, guestCartToken });
+    try {
+      const guestCartToken = context.cart?.value?.token;
+      await context.$spree.api.logIn({ username, password, guestCartToken });
 
-    const cart = await context.$spree.api.getCart();
-    context.setCart(cart);
+      const cart = await context.$spree.api.getCart();
+      context.setCart(cart);
 
-    return {};
+      return {};
+    } catch (e) {
+      if (extractApiErrorSummary(e) === 'invalid_grant') {
+        throw new Error('Invalid username or password');
+      }
+
+      throw e;
+    }
   },
 
   changePassword: async (context: Context, { newPassword }) => {
