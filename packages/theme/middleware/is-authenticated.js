882c566c7b4d6cb16d6a165b71ba87810fe72da0
Rafal Cymerys <rafal@upsidelab.io>
2021-10-22 15:57:59 +0200

    feat(theme): add is-authenticated middleware

---
+++ packages/theme/middleware/is-authenticated.js
@@ -0,0 +1,15 @@
+export default (context) => {
+  const token = context.$cookies.get('spree-bearer-token');
+
+  if (token) {
+    const tokenExpiresAt = token.created_at + token.expires_in;
+    const currentTime = Date.now() / 1000;
+
+    if (currentTime < tokenExpiresAt) {
+      return;
+    }
+  }
+
+  context.app.router.push('/');
+  context.redirect('/');
+};
