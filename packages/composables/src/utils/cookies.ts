bd25ab5071c37a63e0fc9d867cfc3c504ecbcaf6
Diego Lezana <diego.lezana@outlook.com>
2021-12-07 09:40:51 +0100

    feat: get products by cookie

---
+++ packages/composables/src/utils/cookies.ts
@@ -0,0 +1,5 @@
+export const getCurrencyCookie = (context: any) => {
+  const value = `; ${context.$spree.config.axios.headers.cookie}`;
+  const parts = value.split(`; vsf-currency=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+};
