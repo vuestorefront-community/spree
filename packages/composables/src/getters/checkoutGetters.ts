58360ac0cf95f3b6aa1f8279fecd3dd5bad2a3d8
Daniel Hajduk <daniel.hajduk@upsidelab.io>
2021-12-07 14:54:19 +0100

    feat: update checkout getters

--- packages/composables/src/getters/checkoutGetters.ts
+++ packages/composables/src/getters/checkoutGetters.ts
@@ -1,14 +1,13 @@
-import { ShippingMethod } from '@vue-storefront/spree-api';
 import { ShippingRate } from '@vue-storefront/spree-api/src/types';
 
-export const getShippingMethodId = (shippingMethod: ShippingRate): string => shippingMethod.id;
+export const getShippingMethodId = (shippingMethod: ShippingRate): string => shippingMethod?.id || '';
 
-export const getShippingMethodName = (shippingMethod: ShippingRate): string => shippingMethod.name;
+export const getShippingMethodName = (shippingMethod: ShippingRate): string => shippingMethod?.name || '';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getShippingMethodDescription = (shippingMethod: ShippingMethod): string => '';
+export const getShippingMethodDescription = (shippingMethod: ShippingRate): string => '';
 
-export const getShippingMethodPrice = (shippingMethod: ShippingRate): string => shippingMethod.cost;
+export const getShippingMethodPrice = (shippingMethod: ShippingRate): string => shippingMethod?.cost || '';
 
 export const getFormattedPrice = (price: number) => String(price);
 
