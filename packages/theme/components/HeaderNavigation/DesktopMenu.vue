c66eb444fbf741bbbf6572d3924a6040ef97b974
fpdrozd <fpdrozd@gmail.com>
2022-10-14 14:17:19 +0200

    feat: separate desktop and mobile menus

---
+++ packages/theme/components/HeaderNavigation/DesktopMenu.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="sf-header__navigation desktop-only">
+    <template v-if="isMenuAvailable">
+      <SfHeaderNavigationItem
+        v-for="(category, index) in menu && menu.items"
+        :key="index"
+        class="nav-item"
+        v-e2e="`app-header-url_${category.name || category.label}`"
+        :label="category.name || category.label"
+        :link="isExternalLink(category) ? category.link : getRoute(category)"
+      />
+    </template>
+    <template v-else>
+      The menu will show up here once it's configured and enabled in middleware.config.js
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuItem',
+  props: {
+    menu: {
+      type: Object
+    },
+    isMenuAvailable: {
+      type: Boolean
+    },
+    getRoute: {
+      type: Function
+    },
+    isExternalLink: {
+      type: Function
+    }
+  }
+};
+</script>
---
+++ packages/theme/components/HeaderNavigation/MobileMenu.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <SfModal v-if="isMenuAvailable" :visible="isMobileMenuOpen">
+      <SfAccordion open="" :multiple="false" transition="" showChevron>
+        <template v-for="(cat, i) in menu && menu.items">
+          <SfMenuItem
+            v-if="isExternalLink(cat)"
+            :key="i"
+            :label="cat.name || cat.label"
+            class="sf-accordion-item__header"
+            :link="cat.link"
+          />
+          <SfAccordionItem
+            v-else
+            :key="i"
+            class="nav-item"
+            :header="cat.name || cat.label"
+          >
+            <SfList>
+              <SfListItem>
+                <SfMenuItem
+                  :label="$t('components.header_navigation.all')"
+                  class="sf-header-navigation-item__menu-item"
+                  :link="getRoute(cat)"
+                  @click.native="toggleMobileMenu"
+                />
+              </SfListItem>
+              <SfListItem
+                v-for="(subCat, j) in cat.items"
+                :key="j">
+                <SfMenuItem
+                  :label="subCat.name || subCat.label"
+                  class="sf-header-navigation-item__menu-item"
+                  :link="getRoute(subCat)"
+                  @click.native="toggleMobileMenu"
+                />
+              </SfListItem>
+            </SfList>
+          </SfAccordionItem>
+        </template>
+      </SfAccordion>
+    </SfModal>
+    <SfModal v-else :visible="isMobileMenuOpen">
+      The menu will show up here once it's configured and enabled in middleware.config.js
+    </SfModal>
+  </div>
+</template>
+
+<script>
+import { SfMenuItem, SfModal, SfAccordion, SfList } from '@storefront-ui/vue';
+import { useUiState } from '~/composables';
+
+export default {
+  name: 'MobileMenu',
+  components: {
+    SfMenuItem,
+    SfModal,
+    SfAccordion,
+    SfList
+  },
+  props: {
+    menu: {
+      type: Object
+    },
+    isMenuAvailable: {
+      type: Boolean
+    },
+    getRoute: {
+      type: Function
+    },
+    isExternalLink: {
+      type: Function
+    }
+  },
+  setup() {
+    const { isMobileMenuOpen, toggleMobileMenu } = useUiState();
+
+    return {
+      isMobileMenuOpen,
+      toggleMobileMenu
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.sf-header-navigation-item {
+  ::v-deep &__item--mobile {
+    display: block;
+  }
+}
+.sf-modal {
+  ::v-deep &__bar {
+    display: none;
+  }
+  ::v-deep &__content {
+    padding: var(--modal-content-padding, var(--spacer-base) 0);
+  }
+}
+</style>
