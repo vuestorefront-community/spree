8a99cb3ddcc7a0febed89594333fb2338ba28418
Filip Drozd <fpdrozd@gmail.com>
2022-12-13 13:00:38 +0100

    feat: add TextButton component

---
+++ packages/theme/components/TextButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="text-button" @click="e => $emit('click', e)">
+    <slot />
+  </button>
+</template>
+<script>
+export default {
+  name: 'TextButton'
+};
+</script>
+
+<style lang="scss" scoped>
+.text-button {
+  display: block;
+  border: none;
+  padding: 0;
+  font-size: 1rem;
+  cursor: pointer;
+}
+</style>
