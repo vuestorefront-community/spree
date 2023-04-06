e25b3d67c9675c4be52815c41809c75241e4302b
Filip Drozd <fpdrozd@gmail.com>
2023-04-06 12:24:55 +0200

    refactor: update interfaces names and api response type assertion

--- packages/theme/getStore.ts
+++ packages/theme/getStore.ts
@@ -6,53 +6,52 @@ interface StoreConfig {
 }
 
 /* eslint-disable camelcase */
-export interface StoreAttr {
-  type: string
-  id: string
-  attributes: {
-    name: string
-    url: string
-    meta_description: string | null
-    meta_keywords: string | null
-    seo_title: string | null
-    default_currency: string
-    supported_currencies: string
-    default_locale: string
-    supported_locales: string
+export interface StoreResponse {
+  data: {
+    type: string
+    id: string
+    attributes: {
+      name: string
+      url: string
+      meta_description: string | null
+      meta_keywords: string | null
+      seo_title: string | null
+      default_currency: string
+      supported_currencies: string
+      default_locale: string
+      supported_locales: string
+    }
   }
-}
-interface IStore {
-  data: StoreAttr
 }
 interface Store {
   name: string
   url: string
-  metaDescription?: string
-  metaKeywords?: string
-  seoTitle?: string
   defaultCurrency: string
   supportedCurrencies: string
   defaultLocale: string
   supportedLocales: string
+  metaDescription?: string
+  metaKeywords?: string
+  seoTitle?: string
 }
 
 const apiRoute = '/api/v2/storefront/store';
 
-const deserializeStore = (apiStore: StoreAttr): Store => ({
-  name: apiStore.attributes.name,
-  url: apiStore.attributes.url,
-  metaDescription: apiStore.attributes.meta_description,
-  metaKeywords: apiStore.attributes.meta_keywords,
-  seoTitle: apiStore.attributes.seo_title,
-  defaultCurrency: apiStore.attributes.default_currency,
-  supportedCurrencies: apiStore.attributes.supported_currencies,
-  defaultLocale: apiStore.attributes.default_locale,
-  supportedLocales: apiStore.attributes.supported_locales
+const deserializeStore = ({ data: { attributes } }: StoreResponse): Store => ({
+  name: attributes.name,
+  url: attributes.url,
+  metaDescription: attributes.meta_description,
+  metaKeywords: attributes.meta_keywords,
+  seoTitle: attributes.seo_title,
+  defaultCurrency: attributes.default_currency,
+  supportedCurrencies: attributes.supported_currencies,
+  defaultLocale: attributes.default_locale,
+  supportedLocales: attributes.supported_locales
 });
 
 const getStore = async (config: StoreConfig): Promise<Store> => {
-  const res = await axios.get(urlJoin(config.backendUrl, apiRoute)).then((res) => res.data) as IStore;
-  return deserializeStore(res.data);
+  const { data } = (await axios.get(urlJoin(config.backendUrl, apiRoute))) as { data: StoreResponse };
+  return deserializeStore(data);
 };
 
 export default getStore;
