8b38d60b01311dcd6dfd6c1f1a698a6606f68aba
Filip Drozd <fpdrozd@gmail.com>
2023-04-11 15:42:28 +0200

    refactor: replace axios call with sdk method in getPaymentMethods

--- packages/api-client/src/api/serializers/payment.ts
+++ packages/api-client/src/api/serializers/payment.ts
@@ -1,7 +1,12 @@
-export const deserializePaymentMethods = (method) => ({
+import { IPaymentMethods } from '@spree/storefront-api-v2-sdk';
+import { PaymentMethods } from '../../types/checkout';
+
+export const deserializePaymentMethods = (
+  methods: IPaymentMethods
+): PaymentMethods => methods.data.map((method) => ({
   id: method.id,
   type: method.attributes.type,
   label: method.attributes.name,
   description: method.attributes.description,
   preferences: method.attributes.preferences
-});
+}));
